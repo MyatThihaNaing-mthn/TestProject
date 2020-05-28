2086b6e6705b7ac1242cf3841baeae67b1eaeb7d
MyatThihaNaing <myatthihanaing.mthn@gmail.com>
2020-05-28 10:33:00 +0630

    Third Commit

---
+++ List.py
@@ -0,0 +1,29 @@
+#List.py
+
+List = []
+
+word = 'Programming'
+
+P   r   o   g   r   a   m   m   i   n   g
+0   1   2   3   4   5   6   7   8   9   10
+-11 -10 -9  -8  -7  -6  -5 -4  -3   -2  -1
+
+#index
+
+word[0]
+word[-2]
+word[3:5]
+word[4:9]
+word[:5]
+word[8:]
+word[5:-3]
+word[:2] + word[3:]
+
+len(word)
+
+square = 'Square'
+len(square)
+len(word) + len(square)
+
+print(word[0:])
+print(word[::-1])
